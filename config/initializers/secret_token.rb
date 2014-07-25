97dac5fa499a16891f7b8408a3a21ac92874ffb0
coslars <coslars@hotmail.com>
2014-07-24 20:13:13 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PlayApp::Application.config.secret_key_base = 'b54b9e73599eef98f9b2219799401088434275a6c5936a9637935555d74472aaf9284bab45f6613194eebaf71eb99ce36bd0bb7f516752af7c9c7cb1777cf0a0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PlayApp::Application.config.session_store :cookie_store, key: '_play_app_session'
